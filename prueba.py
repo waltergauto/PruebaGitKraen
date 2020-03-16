90173729a6cc0d551757ceb2fac6a6d2007c1712
waltergauto <waltergautofcb@fpuna.edu.py>
2020-03-16 20:11:35 -0300

    Se agregaron las funciones mensaje y suma

--- prueba.py
+++ prueba.py
@@ -1,1 +1,18 @@
-print "Hola, este es un repositorio para aprender a usar GitKraken"
+
+def mensaje():
+
+    print ("Hola, este es un repositorio para aprender a usar GitKraken")
+
+    print ("Estamos aprendiendo Python")
+
+mensaje()
+
+def suma(n1, n2):
+
+    return n1+n2
+
+numero1 = 5
+numero2 = 7
+
+print ("Si sumamos " + str(numero1) + " y " + str(numero2) + " obtenemos = ", suma(numero1, numero2))
+
