eeebc06e0ca1bf472fd39ea4742dbdca3a6107cd
waltergauto <waltergautofcb@fpuna.edu.py>
2020-03-16 20:18:58 -0300

    Se ha creado un nuevo archivo de prueba con las funciones suma, resta, multiplicacion y division

---
+++ prueba2.py
@@ -0,0 +1,24 @@
+print ("Hola a todos, este es el segundo archivo de prueba")
+
+def suma(numero1, numero2):
+    return numero1 + numero2
+
+def resta(numero1, numero2):    
+    return numero1 - numero2
+
+def multiplicacion(numero1, numero2):
+    return numero1*numero2
+
+def division(numero1, numero2):
+    if (numero2 == 0):
+        print ("No se puede realizar una division por cero")
+    elif
+        return numero1/numero2
+
+numero1 = 10
+numero2 = 5
+
+suma(numero1, numero2)
+resta(numero1, numero2)
+multiplicacion(numero1, numero2)
+division(numero1, numero2)
