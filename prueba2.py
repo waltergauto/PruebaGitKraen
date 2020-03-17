b4dbae14ed04a32d1ad416747217cff8bfe99578
waltergauto <waltergautofcb@fpuna.edu.py>
2020-03-16 21:04:02 -0300

    Se corrigieron errores en las  funciones suma, resta, multiplicacion y division, aparte de eso se hizo print en pantalla de los resultdos de cada uno de ellos

--- prueba2.py
+++ prueba2.py
@@ -10,16 +10,16 @@ def multiplicacion(numero1, numero2):
     return numero1*numero2
 
 def division(numero1, numero2):
-    if (numero2 == 0):
-        print ("No se puede realizar una division por cero")
-    else
-        return numero1/numero2
+
+    if numero2 == 0:
+        print("No se puede realizar la division por cero")
+    else:
+        return numero1/numero2    
 
 numero1 = 10
 numero2 = 5
 
-suma(numero1, numero2)
-resta(numero1, numero2)
-multiplicacion(numero1, numero2)
-division(numero1, numero2)
-
+print("Suma de " + str(numero1) + " y " + str(numero2) + " = ", suma(numero1, numero2))
+print("Resta de " + str(numero1) + " y " + str(numero2) + " = ", resta(numero1, numero2))
+print("Multiplicacion de " + str(numero1) + " y " + str(numero2) + " = ", multiplicacion(numero1, numero2))
+print("Division de " + str(numero1) + " y " + str(numero2) + " = ", division(numero1, numero2))
